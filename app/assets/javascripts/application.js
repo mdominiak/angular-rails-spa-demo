5f2b91990843bcf10293f1a3b76f51e0c90bc66f
Mateusz Dominiak <mateusz.dominiak@gmail.com>
2014-12-03 10:41:14 +0100

    DELETE /api/users/sign_out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require 'bootstrap-sass-official/dropdown'
 //= require angular/angular
 //= require angular-route/angular-route
 //= require_tree .
--- spec/requests/api/sessions_spec.rb
+++ spec/requests/api/sessions_spec.rb
@@ -3,16 +3,30 @@ require 'rails_helper'
 describe "Sessions API", type: :request do
   let!(:user) { FactoryGirl.create(:user) }
 
-  it 'successful authentication' do
-    post "/api/users/sign_in.json", user: { email: user.email, password: user.password }
-    expect(response).to have_http_status(:created)
-    json = JSON.parse(response.body)
-    expect(json).to eq({'email' => user.email})
+  describe 'login' do
+    it 'success' do
+      post "/api/users/sign_in", user: { email: user.email, password: user.password }
+      expect(response).to have_http_status(:created)
+      json = JSON.parse(response.body)
+      expect(json).to eq({'email' => user.email})
+    end
+
+    it 'with invalid credentials' do
+      post "/api/users/sign_in", email: user.email, password: 'invalid'
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 
-  it 'authentication with invalid credentials' do
-    post "/api/users/sign_in.json", email: user.email, password: 'invalid'
-    expect(response).to have_http_status(:unauthorized)
+  describe 'logout' do
+    before do
+      post "/api/users/sign_in", user: { email: user.email, password: user.password }      
+    end
+
+    it 'success' do
+      delete "/api/users/sign_out"
+      expect(response).to have_http_status(204)
+      expect(response.body).to be_empty
+    end
   end
 
 end
