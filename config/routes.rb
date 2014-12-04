edd0d4cb05af636cae8875421f8b541e389fb845
Mateusz Dominiak <mateusz.dominiak@gmail.com>
2014-12-04 14:47:57 +0100

    /meals/new route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'sign_up' => 'home#index'
   get 'login' => 'home#index'
   get 'dashboard' => 'home#index'
+  get 'meals/new' => 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
